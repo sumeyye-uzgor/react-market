f5dcb1983343ece484df25957059481230409889
Sümeyye Üzgör <65453213+sumeyye-uzgor@users.noreply.github.com>
2021-09-30 04:18:46 +0300

    general layout design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 function App() {
   return (
     <div className="flex flex-col items-center" style={{ minHeight: "100vh" }}>
-      <div className="h-16 sticky top-0 left-0 w-full bg-red-100">Navbar</div>
-      <div className="bg-blue-500 font-bold text-3xl w-full flex flex-col justify-center items-center flex-grow">
-        {" "}
-        Hello
+      <div className="h-16 sticky top-0 left-0 w-full bg-primary-blue text-primary-white">
+        Navbar
       </div>
-      <div className="h-16 w-full bg-red-200">Footer</div>
+      <div className="font-bold text-3xl w-full flex flex-col items-center flex-grow py-16 bg-bg-gray">
+        <div className="w-3/12"></div>
+        <div className="flex flex-col w-6/12">
+          <div className="font-normal text-text-gray">Products</div>
+          <div className="flex flex-row mt-3">
+            <div className="px-3 py-1 bg-primary-blue text-primary-white font-light text-lg">
+              ABC
+            </div>
+          </div>
+          <div className="w-full bg-bg-white mt-5">DEnem</div>
+        </div>
+        <div className="w-3/12"></div>
+      </div>
+      <div className="h-8 w-full text-primary-blue flex flex-row items-center justify-center">
+        &#169;2019 Market . Privacy Policy
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          blue: "#1EA4CE",
+          white: "#FFFFFF",
+          black: "#000000",
+          gray: "#1EA4CE",
+        },
+        bg: {
+          gray: "#E5E5E5",
+          white: "#F3F0FE",
+        },
+        text: {
+          gray: "#6F6F6F",
+          black: "#191919",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
